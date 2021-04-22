bb52694835c5c762136650427f30f7ab12aa7cdc
aurelio <aurelioarcabascio@gmail.com>
2021-04-22 04:39:02 -0400

    added antd and react router

---
+++ README.md
@@ -0,0 +1,5 @@
+### Guild Recruitr
+<hr />
+
+Help players find raiding and/or PvP guilds on World of Warcraft<br /><br />
+You can see a preview <a href="https://aurelio4.github.io/guild_recruit/">here</a>
--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 const App = () => {
   return (
-      <h1>App Test</h1>
+      <Router>
+				<ul>
+					<li><Link to='/'>Home</Link></li>
+					<li><Link to='/profile'>Profile</Link></li>
+					<li><Link to='/create-guild'>Create Guild</Link></li>
+				</ul>
+
+				<Switch>
+					<Route path="/profile">
+						<h1>profile path</h1>
+					</Route>
+					<Route path="/create-guild">
+						<h1>create guild path</h1>
+					</Route>
+					<Route path="/">
+						<h1>homepage</h1>
+					</Route>
+				</Switch>
+			</Router>
   )
 }
 
