7ae32ead302874195571cf1cd4c8ea814b732fb1
aurelio <aurelioarcabascio@gmail.com>
2021-05-05 02:38:20 -0400

    Fixed up guild card

--- src/components/common/GuildCard.js
+++ src/components/common/GuildCard.js
@@ -2,16 +2,20 @@ import React, { useState } from 'react'
 import { Card } from 'antd';
 
 const GuildCard = () => {
-	const [guildNameClassName, setGuildNameClassName] = useState('guild-name');
+	const [guildNameClassName, setGuildNameClassName] = useState('guild-name')
+	const [guildMasterClassName, setGuildMasterClassName] = useState('guild-master')
 
 	return (
 		<div className="guild-card-container">
 			<Card className="guild-card">
 				<h1 className={guildNameClassName}>testing</h1>
-				<h2 className="region-and-server">US-Bigglesworth</h2>
-				<h2 className="guild-master">GM: chelkuhs</h2>
+				<h2 className={guildMasterClassName}>GM: chelkuhs</h2>
+				<h2 className="region-and-server">US-Bigglesworth (PvE)</h2>
+				<h2 className="guild-pop">Population: Medium (51-100)</h2>
 				<hr className="hr-divider" />
-				<p className="guild-bio">Card content</p>
+				<p className="guild-bio">Test Description for a Test Guild. I wonder if this text will wrap over or just keep going and going and going and going and going</p>
+				<hr className="hr-divider" />
+				<button>Apply</button>
     	</Card>
 		</div>
 	)
